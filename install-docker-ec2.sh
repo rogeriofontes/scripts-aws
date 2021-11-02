3623cce3710873f5cb2ccebba9c246bd671cb13b
Rogério Fontes <fontestz@gmail.com>
2021-11-02 20:35:49 -0300

    Update install-docker-ec2.sh

--- install-docker-ec2.sh
+++ install-docker-ec2.sh
@@ -6,4 +6,6 @@ sudo amazon-linux-extras install docker -y
 sudo usermod -aG docker ec2-userd
 sudo service docker start
 sudo chmod 777 /var/run/docker.sock
-docker run -it --rm -d -p 8080:80 --name web -v ~/site-content:/usr/share/nginx/html nginx
+docker run -it -d -p 80:80 --name nginx -v ~/site-content/nginxconf:/etc/nginx/conf.d -v ~/site-content/server1:/usr/share/nginx/html nginx
+#docker run -it -d -p 80:80 --name nginx -v ~/site-content/nginxconf:/etc/nginx/conf.d -v ~/site-content/server1:/usr/share/nginx/html nginx
+
